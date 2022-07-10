7e50f154408bf632d33e8a3b196ad3b235fb00ba
Carlos Andrade <krlosgilson@gmail.com>
2022-07-10 15:47:16 -0300

    Change user model specs

--- app/models/user.rb
+++ app/models/user.rb
@@ -1,2 +1,4 @@
 class User < ApplicationRecord
+  validates :name, presence: true, length: { maximum: 80 }
+  validates :email, presence: true, uniqueness: true, length: { maximum: 80 }  
 end
--- spec/models/user_spec.rb
+++ spec/models/user_spec.rb
@@ -1,5 +1,24 @@
 require 'rails_helper'
 
-RSpec.describe User, type: :model do
-  pending "add some examples to (or delete) #{__FILE__}"
+describe User do
+  subject {
+    described_class.new(name: Faker::Name.name,
+                        email: Faker::Internet.email)
+  }
+
+  describe 'Validations' do
+    it "is valid with valid attributes" do
+      expect(subject).to be_valid
+    end
+
+    it 'is not valid without a name' do
+      subject.name = nil
+      expect(subject).to_not be_valid
+    end
+
+    it 'is not valid without a email' do
+      subject.email = nil
+      expect(subject).to_not be_valid
+    end
+  end
 end
