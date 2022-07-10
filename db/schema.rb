2eb86d2f684def14dbcdf383748e81bae0dabd0e
Carlos Andrade <krlosgilson@gmail.com>
2022-07-10 11:26:27 -0300

    Add User model

--- db/schema.rb
+++ db/schema.rb
@@ -10,5 +10,12 @@
 #
 # It's strongly recommended that you check this file into your version control system.
 
-ActiveRecord::Schema[7.0].define(version: 0) do
+ActiveRecord::Schema[7.0].define(version: 2022_07_10_140040) do
+  create_table "users", force: :cascade do |t|
+    t.string "name", limit: 80
+    t.string "email", limit: 100
+    t.datetime "created_at", null: false
+    t.datetime "updated_at", null: false
+  end
+
 end
