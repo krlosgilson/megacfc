05653336206ca8691b538746694c9bea41f8c2ce
Carlos Andrade <krlosgilson@gmail.com>
2022-07-10 15:22:18 -0300

    Change limit of columns of user

--- db/migrate/20220710140040_create_users.rb
+++ db/migrate/20220710140040_create_users.rb
@@ -2,7 +2,7 @@ class CreateUsers < ActiveRecord::Migration[7.0]
   def change
     create_table :users do |t|
       t.string :name, limit: 80
-      t.string :email, limit: 100
+      t.string :email, limit: 80
 
       t.timestamps
     end
